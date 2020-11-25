2f020136653fd887c8bb4aac5bf68b5b20a51d67
diego <diego@LAPTOP-52U6NT9I>
2020-11-25 10:19:33 +0100

    base de datos

--- public_html/js/registro.js
+++ public_html/js/registro.js
@@ -3,5 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var bd;
+var mostrarerror;
+var comenzar;
 
+function crearbd(evento) {
+    var basededatos = evento.target.result;
+    var usuarios = basededatos.createObjectStore("usuarios", {keyPath: "dni"});
+    usuarios.createIndex("BuscarEmail", "email", {unique:true});
+    var viajes = basededatos.createObjectStore("viajes", {keyPath: "id", autoIncrement:true});
+    viajes.createIndex("fechaHora", "fechaHora", {unique:true});
+}
 
+function iniciar() {
+    var solicitud = indexedDB.open("arabaCar05");
+    solicitud.addEventListener("error", mostrarerror);
+    solicitud.addEventListener("success", comenzar);
+    solicitud.addEventListener("upgradeneeded", crearbd);
+}
+
+window.addEventListener("load", iniciar);
