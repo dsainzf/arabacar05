d441a5302dcafa7497dcb1eaf05a353ffeafd0a8
diego <diego@LAPTOP-52U6NT9I>
2020-11-28 19:40:13 +0100

    mostrar viajes

--- public_html/js/verViajesPubicados.js
+++ public_html/js/verViajesPubicados.js
@@ -5,32 +5,27 @@
  */
 
 var cajadatos, bd;
+
 function iniciar() {
+    var boton = document.getElementById("verViaje");
+    boton.addEventListener("click", mostrar);
+    cajadatos = document.getElementById("cajadatos");
+    
+    
     var solicitud = indexedDB.open("arabaCar05");
     solicitud.addEventListener("error", mostrarerror);
     solicitud.addEventListener("success", comenzar);
     solicitud.addEventListener("upgradeneeded", crearbd);
-    var boton = document.getElementById("verViaje");
-    boton.addEventListener("click", mostrar());
 }
-/*function iniciar() {
-  cajadatos = document.getElementById("cajadatos");
-  var boton = document.getElementById("grabar");
-  boton.addEventListener("click", agregarobjeto);
-
-  var solicitud = indexedDB.open("basededatos");
-  solicitud.addEventListener("error", mostrarerror);
-  solicitud.addEventListener("success", comenzar);
-  solicitud.addEventListener("upgradeneeded", crearbd);
-}*/
 
 function mostrarerror(evento) {
   alert("Error: " + evento.code + " " + evento.message);
 }
 function comenzar(evento) {
   bd = evento.target.result;
-  mostrar();
+  //mostrar();
 }
+
 function crearbd(evento) {
     var basededatos = evento.target.result;
     var usuarios = basededatos.createObjectStore("usuarios", {keyPath: "dni"});
@@ -42,27 +37,25 @@ function crearbd(evento) {
 function mostrar() {
   
   cajadatos.innerHTML = "";
-  var transaccion = bd.transaction(["viajes"], "readwrite");
+  var transaccion = bd.transaction(["viajes"]);
   var almacen = transaccion.objectStore("viajes");
-  transaccion.addEventListener("complete", completado);
-  transaccion.addEventListener("error", error);
   var puntero = almacen.openCursor();
   puntero.addEventListener("success", mostrarlista);
 }
-function mostrarlista(evento) {
+function mostrarlista(evento) 
+{
   var puntero = evento.target.result;
+  var fechaActual = document.getElementById("tiempolocal").value;
+  var usuario = JSON.parse(sessionStorage.getItem("usuario"));
+  var dni = usuario.dni
   if (puntero) {
-     //cajadatos.innerHTML += "<div>" + clave + " - " + valor + "</div>";
-    cajadatos.innerHTML += "<div>" + puntero.value.listaValoresOrig + " - " + puntero.value.listaValoresDest + " - " + puntero.value.tiempolocal + "</div>";
-    puntero.continue();
+      if( fechaActual < puntero.value.tiempolocal && puntero.value.dni === dni)
+      {
+           cajadatos.innerHTML += "<div>" + puntero.value.listaValoresOrig + " - " + puntero.value.listaValoresDest + " - " + puntero.value.tiempolocal + "</div>";
+            
+      }
+      puntero.continue();
   }
 }
 
-function error(){
- alert ("error");
- }
-function completado(){
- alert ("completado");
- location.href = "verViajesPublicados.html";
-}
 window.addEventListener("load", iniciar);
