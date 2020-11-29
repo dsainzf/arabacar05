11f05157e9cc3f49353486f039fc2b18b06cbf32
diego <diego@LAPTOP-52U6NT9I>
2020-11-29 17:02:37 +0100

    registro

--- public_html/js/registro.js
+++ public_html/js/registro.js
@@ -24,7 +24,31 @@ function iniciar() {
     solicitud.addEventListener("error", mostrarerror);
     solicitud.addEventListener("success", comenzar);
     solicitud.addEventListener("upgradeneeded", crearbd);
-    var boton = document.getElementById("botonCrear");
+    
+    nombre = document.getElementById("nombre");
+    nombre.addEventListener("input", comprobacionRegistro);
+
+    dni = document.getElementById("dni");
+    dni.addEventListener("input", comprobacionRegistro);
+
+    edad = document.getElementById("edad");
+    edad.addEventListener("input", comprobacionRegistro);
+
+    email = document.getElementById("email");
+    email.addEventListener("input", comprobacionRegistro);
+
+    contraseña = document.getElementById("contraseña");
+    contraseña.addEventListener("input", comprobacionRegistro);
+    
+    coche = document.getElementById("coche");
+    coche.addEventListener("input", comprobacionRegistro);
+
+    cajadatos = document.getElementById("cajadatos");
+
+    //var archivos = document.getElementById("imagen");
+    //archivos.addEventListener("change", procesar);
+
+    var boton = document.getElementById("botonRegistrarse");
     boton.addEventListener("click", guardarUsuario);
 }
 function guardarUsuario() {
@@ -36,7 +60,6 @@ function guardarUsuario() {
   var foto = document.getElementById("foto").value;
   var coche = document.getElementById("coche").value;
   
-
 
   var transaccion = bd.transaction(["usuarios"], "readwrite");
   var almacen = transaccion.objectStore("usuarios");
@@ -52,32 +75,105 @@ function guardarUsuario() {
   document.getElementById("foto").value = "";
   document.getElementById("coche").value = "";
   
-  /*document.querySelector("#mifoto").addEventListener("change", function(){
-    const reader = new FileReader();
-                
-        reader.addEventListener("load", () => {
-        /*console.log(reader.result);
-        localStorage.setItem("recent-image",reader.result);
-        });
-                
-        reader.readAsDataURL(this.files[0]);
-        });
-            
-        document.addEventListener("DOMContentLoaded", () => {
-            const recentImageDataUrl = localStorage.getItem("recent-image");
-                
-            if (recentImageDataUrl){
-                document.querySelector("#imgPreview").setAttribute( "src",recentImageDataUrl);
-            }
-        });*/
-  
  function error(){
  alert ("error");
  }
+ 
+ 
+ 
 function completado(){
  alert ("completado");
  location.href = "index.html";
 }
 
+}
+function comprobacionRegistro()
+{
+    comprobarNombre(nombre.value);
+    comprobarDNI(dni.value);
+    comprobarEdad(edad.value);
+    comprobarEmail(email.value);
+    comprobarContraseña(contraseña.value);
+    comprobarContraseña(coche.value);
+    
+    if(comprobarNombre(nombre.value)&& comprobarDNI(dni.value) && comprobarMovil(edad.value) && comprobarEmail(email.value)
+            && comprobarContraseña(contraseña.value) && comprobarContraseña(coche.value) === true)
+    {
+        return true;
+    }   
+}
+
+function comprobarEmail(pEmail)
+{
+    var ex = /^([a-zA-Z]+[a-zA-Z0-9._-]*)@{1}([a-zA-Z0-9\.]{2,})\.([a-zA-Z]{2,3})$/;
+   
+    if (ex.test(pEmail) || pEmail === '')
+    {
+        email.style.background = '#FFFFFF';
+        return true;
+    } else
+    {
+        email.style.background = '#FFDDDD';
+        return false;
+    }
+}
+
+function comprobarContraseña(pContraseña)
+{
+    var er = /^[a-zA-Z0-9]{4,16}$/;
+    
+    if (er.test(pContraseña) || pContraseña === '')
+    {
+        contraseña.style.background = '#FFFFFF';
+        return true;
+    } else
+    {
+        contraseña.style.background = '#FFDDDD';
+        return false;
+    }
+}
+
+function comprobarEdad(pEdad)
+{
+    
+    if (pEdad > 18)
+    {
+        pEdad.document.write("edad correcta");
+        return true;
+    } else
+    {
+        pEdad.document.write("edad inferiro a 18");
+        return false;
+    }
+}
+
+function comprobarDNI(pDNI)
+{
+    var er = /^[0-9]{8}$/;
+    
+    if (er.test(pDNI) || pDNI === '')
+    {
+        dni.style.background = '#FFFFFF';
+        return true;
+    } else
+    {
+        dni.style.background = '#FFDDDD';
+        return false;
+    }
+}
+
+function comprobarNombre(pNombre)
+{
+    var er = /^[a-zA-Z]{3,12}$/;
+    
+    if (er.test(pNombre) || pNombre === '')
+    {
+        nombre.style.background = '#FFFFFF';
+        return true;
+    } else
+    {
+        nombre.style.background = '#FFDDDD';
+        return false;
+    }
 }
 window.addEventListener("load", iniciar);
